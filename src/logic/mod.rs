10d5d960d0c9891e870670f625d5496e0c7c9996
exepta <genshin.exepta@outlook.com>
2024-05-30 23:01:46 +0200

    feat: begin development of logic.

---
+++ src/logic/mod.rs
@@ -0,0 +1,15 @@
+pub fn calculate(way: &str) -> &str {
+    let mut result :&str = "";
+    if !way.is_empty() {
+
+    }
+    return result;
+}
+
+/** Enum for handling math operators like + - * / */
+enum Operators {
+    ADDITION,
+    SUBTRACTION,
+    MULTIPLICATION,
+    DIVIDED
+}
